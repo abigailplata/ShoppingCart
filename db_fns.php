c0a40c1d36938c520011f4b6501179601320678d
gabrielaliera <95945032+gabrielaliera@users.noreply.github.com>
2022-05-23 14:11:50 -0700

    Add files via upload

---
+++ db_fns.php
@@ -0,0 +1,24 @@
+<?php
+//Gaby fixed 5_23
+
+function db_connect() {
+                                    //username, password, database
+   $result = new mysqli('localhost', 'sticker_sc', 'password', 'sticker_sc');
+   if (!$result) {
+      return false;
+   }
+   $result->autocommit(TRUE);
+   return $result;
+}
+
+function db_result_to_array($result) {
+   $res_array = array();
+
+   for ($count=0; $row = $result->fetch_assoc(); $count++) {
+     $res_array[$count] = $row;
+   }
+
+   return $res_array;
+}
+
+?>
